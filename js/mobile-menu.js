4015145d99450e07d3a4782d87883c78a296a25c
Brandon Hang <brandonshang@gmail.com>
2016-04-05 15:55:42 -0400

    Script to operate mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-	$(document).on('click', '#mobile-menu', function() {
-		$('#mobile-menu').stop().toggleClass('menu-open');
-		$('#mobile-menu').stop().toggleClass('menu-close');
-		$('#menu-links li').stop().toggle(250);
-	});
-	
-	$(document).on('click', '#menu-links li', function() {
-		$('#mobile-menu').stop().toggleClass('menu-open');
-		$('#mobile-menu').stop().toggleClass('menu-close');
-		$('#menu-links li').stop().toggle(250);
+	$(document).on('click', '#menu-button', function() {
+		$(this).toggleClass('menu-open');
+		$('#links li').toggleClass('show-menu');
 	});
 });
