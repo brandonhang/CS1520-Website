f311251927c8f8fbbeccf2a8956b769aa744af83
Brandon Hang <brandonshang@gmail.com>
2016-04-05 15:55:17 -0400

    Added small scroll effect

--- js/easing_nav_jump.js
+++ js/easing_nav_jump.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-	$(document).on('click', '#menu-links a', function(event) {
+	$(document).on('click', '.nav-container a', function(event) {
 		var section = $(this);
 		$('html, body').stop().animate({
 			scrollTop: $(section.attr('href')).offset().top
 		}, 1608, 'easeInOutQuint');
 		event.preventDefault();
+	});
+	
+	$(window).scroll(function() {
+		if ($('.nav-container').offset().top > 50) {
+			$('.blue-menu').fadeIn("slow");
+		}
+		else {
+			$('.blue-menu').fadeOut("slow");
+		}
 	});
 });
