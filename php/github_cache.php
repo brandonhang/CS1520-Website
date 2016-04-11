0816f0f85d829278f3f4612be17a9cd2b771f8f6
Brandon Hang <brandonshang@gmail.com>
2016-04-10 23:25:58 -0400

    Script to update github_cache.txt

---
+++ php/github_cache.php
@@ -0,0 +1,20 @@
+<?php
+	if (empty($_POST)) {
+		include "404.php";
+		die("Unauthorized access prohibited!  That means you!");
+	}
+	if (isset($_POST["avatar"])) {
+		include "404.php";
+		die("Unauthorized access prohibited!  That means you!");
+	}
+	
+	$file = fopen("../misc/github_cache.txt", "w");
+	
+	if (flock($file, LOCK_EX)) {
+		foreach($_POST as $string) {
+			fwrite($file, $string . "\n");
+		}
+	}
+	
+	fclose($file);
+?>
